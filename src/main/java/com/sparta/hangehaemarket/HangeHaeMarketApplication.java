670d52459dc285fe66c4843ccacf280cb1e5d573
Zua <gimzua0228@gmail.com>
2024-07-08 10:31:07 +0900

    first commit

---
+++ src/main/java/com/sparta/hangehaemarket/HangeHaeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hangehaemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangeHaeMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangeHaeMarketApplication.class, args);
+    }
+
+}
